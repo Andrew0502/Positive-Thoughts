1a2a2ed9ea274dd21d70df6afb8742e798bac69f
Insomnia <jamie.mar.jones14@gmail.com>
2020-11-06 13:25:05 -0500

    added form for the sign up page. No CSS yet

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -2,9 +2,30 @@ import React from 'react';
 
 const SignUp = () => {
     return (
-        <div>
-            <h1>Sign Up</h1>
+        <main className="content">
+        <div className="container">
+            <section className="row">
+                <div className="col-sm-12">
+                    <h1>Sign up</h1>
+                </div>
+            </section>
+            <section className="row">
+                <div className="col-sm-12">
+                <form>
+                    <div class="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"/>
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Create Account</button>
+                    </form>
+                </div>
+            </section>
+
         </div>
+
+    </main>
     );
 };
 
