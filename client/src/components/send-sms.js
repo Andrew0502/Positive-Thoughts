47d53d1c5b5f60fae72aebcd1c0950426913c003
Andrew0502 <agcmarcus@gmail.com>
2020-11-06 14:00:09 -0500

    Changed authToken.

--- client/src/components/send-sms.js
+++ client/src/components/send-sms.js
@@ -1,14 +1,16 @@
 const accountSid = "AC53df991b77b438e82d02c2c9dc24e9eb";
-const authToken = "7d03ba852011c5ff998ae78ab56c71de";
+const authToken = "ea18844c33362da0e49e461578494bde";
 
 // const accountSid = process.env.TWILIO_ACCOUNT_SID;
 // const authToken = process.env.TWILIO_ACCOUNT_TOKEN;
 
 const client = require('twilio')(accountSid, authToken);
+console.log("test");
 
 client.messages.create({
     to: "+14233098896",
     from: "+12513143409",
-    body: "Hello World!"
+    body: "test!"
 })
 .then((message) => console.log(message.sid))
+.catch((error) => console.log(error))
