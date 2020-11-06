a1f192ca50d39c53c4ac4ebab2a156027015c252
Insomnia <jamie.mar.jones14@gmail.com>
2020-11-06 13:20:38 -0500

    added login form for sign in page. No CSS added yet

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -3,9 +3,31 @@ import "./SignInCSS.css"
 
 const SignIn = () => {
     return (
-        <div>
-            <h1>Sign In</h1>
-        </div>
+            <main className="content">
+                <div className="container">
+                    <section className="row">
+                        <div className="col-sm-12">
+                            <h1>WELCOME TO POSITIVE THOUGHTS</h1>
+                        </div>
+                    </section>
+                    <section className="row">
+                        <div className="col-sm-12">
+                        <form>
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"/>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Login</button>
+                            <button type="submit" class="btn btn-primary">Sign Up</button>
+                            </form>
+                        </div>
+                    </section>
+
+                </div>
+
+            </main>
     );
 };
 
