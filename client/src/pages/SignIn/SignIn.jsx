0972bcec687286cb28bf9373a89f16e1b3d38e4d
Insomnia <jamie.mar.jones14@gmail.com>
2020-11-06 13:39:48 -0500

    I'm not sure what I did

--- client/src/pages/SignIn/SignIn.jsx
+++ client/src/pages/SignIn/SignIn.jsx
@@ -13,14 +13,14 @@ const SignIn = () => {
                     <section className="row">
                         <div className="col-sm-12">
                         <form>
-                            <div class="form-group">
-                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"/>
+                            <div className="form-group">
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"/>
                             </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                            <div className="form-group">
+                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
                             </div>
-                            <button type="submit" class="btn btn-primary">Login</button>
-                            <button type="submit" class="btn btn-primary">Sign Up</button>
+                            <button type="submit" className="btn btn-primary">Login</button>
+                            <button type="submit" className="btn btn-primary">Sign Up</button>
                             </form>
                         </div>
                     </section>
--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -12,13 +12,13 @@ const SignUp = () => {
             <section className="row">
                 <div className="col-sm-12">
                 <form>
-                    <div class="form-group">
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"/>
+                    <div className="form-group">
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email"/>
                     </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    <div className="form-group">
+                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
                     </div>
-                    <button type="submit" class="btn btn-primary">Create Account</button>
+                    <button type="submit" className="btn btn-primary">Create Account</button>
                     </form>
                 </div>
             </section>
@@ -29,4 +29,4 @@ const SignUp = () => {
     );
 };
 
-export default SignUp;<h1>Sign Up</h1>
+export default SignUp;
