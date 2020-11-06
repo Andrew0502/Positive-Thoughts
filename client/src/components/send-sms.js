d63732197c3eef2b20e27ea810c9d55fc2b721c1
Andrew0502 <agcmarcus@gmail.com>
2020-11-06 14:33:53 -0500

    Added process.env for twilio variables. Added cron job in server.js.

--- client/src/components/send-sms.js
+++ client/src/components/send-sms.js
@@ -1,16 +1,14 @@
-const accountSid = "AC53df991b77b438e82d02c2c9dc24e9eb";
-const authToken = "ea18844c33362da0e49e461578494bde";
+require ("dotenv").config()
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_ACCOUNT_TOKEN;
+// const client = require('twilio')(accountSid, authToken);
 
-// const accountSid = process.env.TWILIO_ACCOUNT_SID;
-// const authToken = process.env.TWILIO_ACCOUNT_TOKEN;
-
-const client = require('twilio')(accountSid, authToken);
 console.log("test");
 
-client.messages.create({
-    to: "+14233098896",
-    from: "+12513143409",
-    body: "test!"
-})
-.then((message) => console.log(message.sid))
-.catch((error) => console.log(error))
+// client.messages.create({
+//     to: "+14233098896",
+//     from: "+12513143409",
+//     body: "test!"
+// })
+// .then((message) => console.log(message.sid))
+// .catch((error) => console.log(error))
