870af9c13171fdfcbf7eab8d960baa35f969c189
7J647 <007J647@gmail.com>
2020-11-08 15:25:51 -0500

    changed phone numbers text color

--- client/src/pages/Hotlines/Hotlines.jsx
+++ client/src/pages/Hotlines/Hotlines.jsx
@@ -10,19 +10,19 @@ const Hotlines = () => {
             <h1>YOU ARE NOT ALONE</h1>
             <div><br/></div>
             <div><br/></div>
-            <section id="PhoneNumbers">
+            <section id="ContactInfo">
             <div><br/></div>
             <h2>National Suicide Prevention Lifeline Veterans Crisis Line</h2>
-            <h2>1-800-273-8255</h2>
+            <h2 class="PhoneNumbers">1-800-273-8255</h2>
             <div><br/></div>
             <h2>Georgia Crisis &amp; Access Line (GCAL)</h2>
-            <h2>1-800-715-4225</h2>
+            <h2 class="PhoneNumbers">1-800-715-4225</h2>
             <div><br/></div>
             <h2>TrevorLifeline</h2>
-            <h2>1-866-488-7386</h2>
+            <h2 class="PhoneNumbers">1-866-488-7386</h2>
             <div><br/></div>
             <h2>BlackLifeline</h2>
-            <h2>1-800-604-5841</h2>
+            <h2 class="PhoneNumbers">1-800-604-5841</h2>
             <div><br/></div>
             </section>
 
