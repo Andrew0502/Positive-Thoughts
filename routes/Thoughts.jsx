9c7cd4c07735d7c09ebbc222f6b51a776456cdad
dagreatcode <dagreatcode@gmail.com>
2020-11-08 15:29:29 -0500

    added a thoughts.jsx just in case we need it later

---
+++ routes/Thoughts.jsx
@@ -0,0 +1,16 @@
+// const router = require("express").Router();
+// const promptControllers = require("../../controllers/booksController");
+
+// // Matches with "/api/prompt"
+// router.route("/")
+//   .get(promptControllers.findAll)
+//   .post(promptControllers.create);
+
+// // Matches with "/api/prompt/:id"
+// router
+//   .route("/:id")
+//   .get(promptControllers.findById)
+//   .put(promptControllers.update)
+//   .delete(promptControllers.remove);
+
+// module.exports = router;
