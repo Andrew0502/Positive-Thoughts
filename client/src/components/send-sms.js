56513ebeaa322eff4d9c01cbd9b9c4df982b5900
Andrew0502 <agcmarcus@gmail.com>
2020-11-09 14:40:16 -0500

    Made changes to send-sms.js.

--- client/src/components/send-sms.js
+++ client/src/components/send-sms.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const pino = require("express-pino-logger")();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_ACCOUNT_TOKEN;
 const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
@@ -10,21 +9,10 @@ const client = require("twilio")(accountSid, authToken);
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(pino);
 
-app.get("/api/greeting", (req, res) => {
-  const name = req.query.name || "World";
-  res.setHeader("Content-Type", "application/json");
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
+console.log("test");
 
-app.get("/api/greeting", (req, res) => {
-  const name = req.query.name || "World";
-  res.setHeader("Content-Type", "application/json");
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
-
-app.post("/api/messages", (req, res) => {
+app.post("/api/thoughts", (req, res) => {
   res.header("Content-Type", "application/json");
   client.messages
     .create({
@@ -43,6 +31,12 @@ app.post("/api/messages", (req, res) => {
     });
 });
 
+app.get("/api/prompts", (req, res) => {
+  const name = req.query.name || "World";
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+});
+
 app.listen(3001, () =>
   console.log("Express server is running on localhost:3001")
 );
@@ -51,12 +45,12 @@ app.listen(3001, () =>
 
 
 
-console.log("test");
 
-client.messages.create({
-    to: "+14233098896",
-    from: "+12513143409",
-    body: "test!"
-})
-.then((message) => console.log(message.sid))
-.catch((error) => console.log(error))
+
+// client.messages.create({
+//     to: "+14233098896",
+//     from: "+12513143409",
+//     body: "test!"
+// })
+// .then((message) => console.log(message.sid))
+// .catch((error) => console.log(error))
