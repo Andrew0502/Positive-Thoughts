4ca5d966d52d57bd461662172662e97b352c0576
dagreatcode <dagreatcode@gmail.com>
2020-11-12 17:12:34 -0500

    cleanup

--- send-sms.js
+++ send-sms.js
@@ -3,14 +3,14 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_ACCOUNT_TOKEN;
 const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
 const client = require("twilio")(accountSid, authToken);
-const db = require("./models")
+const db = require("./models");
 
-function sendText(message, number){
+function sendText(message, number) {
   client.messages
     .create({
       from: twilioNumber,
       to: number,
-      body: message
+      body: message,
     })
     .then((call) => {
       // console.log(call);
