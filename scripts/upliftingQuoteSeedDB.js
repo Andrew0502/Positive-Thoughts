8c6ccc09df7c467e68cbf8294e374f597846f147
Andrew0502 <agcmarcus@gmail.com>
2020-11-12 18:15:59 -0500

    Finished upliftingQuoteSeed.js

--- scripts/upliftingQuoteSeedDB.js
+++ scripts/upliftingQuoteSeedDB.js
@@ -14,6 +14,34 @@ const upliftingQuotesSeed = [
     message_text:
       "Always keep your eyes open. Keep watching. Because whatever you see can inspire you. — Grace Coddington",
   },
+  {
+    message_text:
+      "There is no way to happiness. Happiness is the way. — Thich Nhat Hanh",
+  },
+  {
+    message_text:
+      "Champions keep playing until they get it right. — Billie Jean King",
+  },
+  {
+    message_text:
+      "A comfort zone is a beautiful place, but nothing ever grows there. — Author Unknown",
+  },
+  {
+    message_text:
+      "You must be the change you wish to see in the world. — Mahatma Gandhi",
+  },
+  {
+    message_text:
+      "Numbing the pain for a while will only make it worse when you finally feel it. — Albus Dumbledore",
+  },
+  {
+    message_text:
+      "If you want to live a happy life, tie it to a goal, not to people or objects. — Albert Einstein",
+  },
+  {
+    message_text:
+      "Never give up on a dream just because of the time it will take to accomplish it. The time will pass anyway. — Earl Nightingale",
+  },
 ];
 
 db.UpliftingQuotes.remove({})
