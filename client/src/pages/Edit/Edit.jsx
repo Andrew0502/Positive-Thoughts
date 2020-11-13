1c8758ff872af5844d85cad21c8dd9c50af89f59
7J647 <007J647@gmail.com>
2020-11-12 21:40:00 -0500

    slight update to edit.jsx

--- client/src/pages/Edit/Edit.jsx
+++ client/src/pages/Edit/Edit.jsx
@@ -3,7 +3,6 @@ import Navbar from "../../components/Navbar";
 import { Link, useParams, useHistory } from "react-router-dom";
 import API from "../../utils/API";
 import "./Edit.css";
-import "../assets/main.css"
 
 const Edit = () => {
   const [user, setUser] = useState({});
@@ -42,102 +41,114 @@ const Edit = () => {
       .catch((err) => console.log(err));
   }, []);
   return (
-    <div>
+    <body>
       <Navbar />
-      <main className="content content-pad">
-        <div className="container cover-container">
-          <section className="row">
-            <div className="col-sm-12">
-              <h1 className="heading">Edit Profile</h1>
-            </div>
-          </section>
-          <div><br/></div>
-          <section className="row">
-            <form className="col-sm-6" onSubmit={handleFormSubmit}>
-                  <input value={user.firstName}
-                  name="firstName" type="text"
-                  className="form-control edit-inputs"
+      <main className="content">
+      <div className="container">
+        <section className="row">
+          <div className="col-sm-12">
+            <h1>Edit Profile</h1>
+          </div>
+        </section>
+        <section className="row">
+          <div className="col-sm-12">
+            <form
+            onSubmit={handleFormSubmit}>
+              <div className="form-group">
+              <input
+                  value={user.firstName}
+                  name="firstName"
+                  type="text"
+                  className="form-control"
                   onChange={handleInputChange}
                   id="enterFirstName"
                   aria-describedby="emailHelp"
-                  placeholder="First Name"/>
-            </form>
-            <form className="col-sm-6" onSubmit={handleFormSubmit}>
+                  placeholder="First Name"
+                />
+              </div>
+              <div className="form-group">
               <input
                   value={user.lastName}
                   name="lastName"
                   type="text"
-                  className="form-control edit-inputs"
+                  className="form-control"
                   onChange={handleInputChange}
                   id="enterLastName"
                   aria-describedby="emailHelp"
                   placeholder="Last Name"
                 />
-            </form>
-            <div className="col-sm-12"><hr/></div>
-            <form className="col-sm-6" onSubmit={handleFormSubmit}>
-            <input
+              </div>
+              <div className="form-group">
+                <input
                   value={user.birthday}
                   name="birthday"
                   type="date"
-                  className="form-control edit-inputs"
+                  className="form-control"
                   onChange={handleInputChange}
                   id="exampleBirthday"
                   aria-describedby="emailHelp"
                   placeholder="birthday"
                 />
-            </form>
-            <form className="col-sm-6" onSubmit={handleFormSubmit}>
-            <input
+              </div>
+              <div className="form-group">
+                <input
                   value={user.phoneNumber}
                   name="phoneNumber"
                   type="text"
-                  className="form-control edit-inputs"
+                  className="form-control"
                   onChange={handleInputChange}
                   id="examplePhoneNumber"
                   placeholder="Phone Number"
                 />
-            </form>
-            <div className="col-sm-12"><hr/></div>
-            <form className="col-sm-6" onSubmit={handleFormSubmit}>
-            <input
+              </div>
+              <div className="form-group">
+                <input
                   value={user.password}
                   name="password"
                   type="password"
-                  className="form-control edit-inputs"
+                  //in twice so a user can confirm password
+                  className="form-control"
                   onChange={handleInputChange}
                   id="examplePassword"
                   placeholder="Password"
                 />
-            </form>
-            <form className="col-sm-6" onSubmit={handleFormSubmit}>
-            <input
+              </div>
+              {/* <div className="form-group">
+                <input
+                  value={user.passwordConfirm}
+                  name="passwordConfirm"
+                  type="password"
+                  className="form-control"
+                  onChange={handleInputChange}
+                  id="exampleConfirmPassword"
+                  placeholder="Confirm Password"
+                />
+              </div> */}
+              <div className="form-group">
+                <input
                   value={user.email}
                   name="email"
                   type="email"
-                  className="form-control edit-inputs"
+                  //only accepts a formatted date
+                  className="form-control"
                   onChange={handleInputChange}
                   id="userEmail"
                   placeholder="Email Address"
                 />
-            </form>
-            </section>
-            <div><br/></div>
-            <section className="row">
-              <div className="col-sm-6">
-            <Link type="submit" className="btn btn-primary edit-btn button" to="/profile">
-              CANCEL
-            </Link>
-            </div>
-            <div className="col-sm-6">
+              </div>
+              <Link type="submit" className="btn btn-primary edit-btn button" to="/profile">
+                CANCEL
+              </Link>
+              
               <button type="submit" className="btn btn-primary edit-btn button">
                 UPDATE ACCOUNT
               </button>
-            </div>
-          </section>
-        </div>
-      </main>
-    </div>
+            </form>
+          </div>
+        </section>
+      </div>
+    </main>
+    </body>
   );
 };
 
