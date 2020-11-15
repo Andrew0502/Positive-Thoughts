e344af166b08477d03b66eaf151bf102fa84ab8a
Andrew0502 <agcmarcus@gmail.com>
2020-11-15 16:40:38 -0500

    Compressed code in server.js and send-sms.js. Removed cron.js.

--- send-sms.js
+++ send-sms.js
@@ -1,24 +1,17 @@
 require("dotenv").config();
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_ACCOUNT_TOKEN;
-const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
-const client = require("twilio")(accountSid, authToken);
-const db = require("./models");
-
-function sendText(message, number) {
+const accountSid = process.env.TWILIO_ACCOUNT_SID,
+  authToken = process.env.TWILIO_ACCOUNT_TOKEN,
+  twilioNumber = process.env.TWILIO_PHONE_NUMBER,
+  client = require("twilio")(accountSid, authToken),
+  db = require("./models");
+function sendText(a, b) {
   client.messages
-    .create({
-      from: twilioNumber,
-      to: number,
-      body: message,
+    .create({ from: twilioNumber, to: b, body: a })
+    .then(() => {
+      console.log("\uD83D\uDC83\uD83D\uDD7A");
     })
-    .then((call) => {
-      // console.log(call);
-      console.log("💃🕺");
-    })
-    .catch((err) => {
-      console.log(err);
+    .catch((a) => {
+      console.log(a);
     });
 }
-
 module.exports = sendText;
